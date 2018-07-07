00d4538c58748392fa368129454538bc024ddaf8
delayalen <alanoneul@gmail.com>
2018-07-06 19:32:38 -0500

    actualizacion 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import {LoginPageComponent} from './componentes/login-page/login-page.component'
 import {RegisterPageComponent} from './componentes/register-page/register-page.component'
 import {PrivadoPageComponent} from './componentes/privado-page/privado-page.component'
 import {NotFoundPageComponent} from './componentes/not-found-page/not-found-page.component'
-
+ 
 const routes: Routes = [
   {path: '', component:HomePageComponent},
 {path:'login', component:LoginPageComponent},
